ebf3c9c31f4833105d3c0196a01473064ea4e516
Alex Tessier <alextess91@gmail.com>
2017-08-29 17:39:27 +0200

    ajout autocomplete

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NEED PC';
+
+  autocomplete: { data: { [key: string]: string } };
+
+  ngOnInit() {
+    this.setAutocomplete();
+  }
+
+  setAutocomplete() {
+    this.autocomplete = {
+      data: {
+        'Lenovo X50': 'assets/img/logo.png',
+        'Macbook pro 2017': 'assets/img/logo.png',
+        'Macbook pro 2015': 'assets/img/logo.png',
+      },
+    };
+  }
 }
