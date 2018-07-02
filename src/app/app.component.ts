b41121daee5e0f594bdbf2ae8a9d68715c258c02
Alex <alextess91@gmail.com>
2018-07-02 17:02:53 +0200

    Oupsy url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,15 +25,15 @@ export class AppComponent {
   }
 
   getComputerList() {
-    this.http.get('http://127.0.0.1:81/api/v1/search/computers/').subscribe(data => {
-      // this.http.get('https://api.needpc.fr/v1/search/computers/').subscribe(data => {
-      // Read the result field from the JSON response.
-      this.results = data['data'];
-      this.computerSearch = this.results;
-    });
-  }
+    // this.http.get('http://127.0.0.1:81/api/v1/search/computers/').subscribe(data => {
+    this.http.get('https://api.needpc.fr/v1/search/computers/').subscribe(data => {
+    // Read the result field from the JSON response.
+    this.results = data['data'];
+    this.computerSearch = this.results;
+  });
+}
 
-  ngOnInit() {
-    this.getComputerList();
-  }
+ngOnInit() {
+  this.getComputerList();
+}
 }
