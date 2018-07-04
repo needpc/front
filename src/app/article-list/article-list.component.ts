548788fdac44070bee8a36ee7daef4653e4bf14a
Alex <alextess91@gmail.com>
2018-07-04 10:54:33 +0200

    Filtre recherche amélioré

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -3,8 +3,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { Globals } from '../globals';
 
-import * as filter from '../filter.json';
-
 declare var jquery:any;
 declare var $ :any;
 
@@ -14,40 +12,72 @@ declare var $ :any;
   styleUrls: ['./article-list.component.css']
 })
 export class ArticleListComponent implements OnInit {
-  filters: Object[];
-  jsonFilter = (<any>filter);
   count: any;
   results: string[];
-  filter1: any;
   noImg = "https://www.dia.org/sites/default/files/No_Img_Avail.jpg";
   activity: any;
 
-  countComputers() {
-    if (this.count == 1) {
-      this.count = this.count + ' résultat correspond à vos critères :';
+  jsonChoiceData: any;
+  OptionQ1: any;
+  OptionR1: any;
+  OptionQ2: any;
+  OptionR2: any;
+  OptionQ3: any;
+  OptionR3: any;
+  OptionQ4: any;
+  OptionR4: any;
+
+  cookie1 = this.cookieService.get('cookie1');
+  cookie2 = this.cookieService.get('cookie2');
+  cookie3 = this.cookieService.get('cookie3');
+  cookie4 = this.cookieService.get('cookie4');
+
+  initOptions() {
+    this.http.get(this.globals.urlRequest+'ask').subscribe(
+      data => {
+        this.jsonChoiceData = data['data'];
+
+        this.OptionQ1 = this.jsonChoiceData[1].quest;
+        this.OptionR1 = this.jsonChoiceData[1].responses;
+
+        this.OptionQ2 = this.jsonChoiceData[2].quest;
+        this.OptionR2 = this.jsonChoiceData[2].responses;
+
+        this.OptionQ3 = this.jsonChoiceData[3].quest;
+        this.OptionR3 = this.jsonChoiceData[3].responses;
+
+        this.OptionQ4 = this.jsonChoiceData[4].quest;
+        this.OptionR4 = this.jsonChoiceData[4].responses;
+      });
     }
-    else if (this.count > 1) {
-      this.count = this.count + ' résultats correspondent à vos critères :';
+
+    countComputers() {
+      if (this.count == 1) {
+        this.count = this.count + ' résultat correspond à vos critères :';
+      }
+      else if (this.count > 1) {
+        this.count = this.count + ' résultats correspondent à vos critères :';
+      }
+      else if (this.count == 0) {
+        this.count = 'Aucun résultat ne correspond à vos critères.';
+      }
     }
-    else if (this.count == 0) {
-      this.count = 'Aucun résultat ne correspond à vos critères.';
+
+    getAllComputers() {
+      // this.http.get(this.globals.urlRequest+'search/computers?activity='+this.cookieService.get('cookie0')).subscribe(data => {
+      this.http.get(this.globals.urlRequest+'search/computers?activity=1').subscribe(
+      data => {
+        this.results = data['data'];
+
+        this.count = this.results.length;
+        this.countComputers();
+      });
+    }
+
+    constructor(private http: HttpClient, private cookieService: CookieService, private globals: Globals) {}
+
+    async ngOnInit() {
+      this.initOptions();
+      this.getAllComputers();
     }
   }
-
-  getAllComputers() {
-    // this.http.get(this.globals.urlRequest+'search/computers?activity='+this.cookieService.get('cookie0')).subscribe(data => {
-    this.http.get(this.globals.urlRequest+'search/computers?activity=1').subscribe(data => {
-      this.results = data['data'];
-
-      this.count = this.results.length;
-      this.countComputers();
-    });
-  }
-
-  constructor(private http: HttpClient, private cookieService: CookieService, private globals: Globals) {}
-
-  async ngOnInit() {
-    this.filters = this.jsonFilter;
-    this.getAllComputers();
-  }
-}
